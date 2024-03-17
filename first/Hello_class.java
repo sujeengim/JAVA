c16b12300be363c41ac98a37f3bf3bc8fa75e45c
Kim Sujin <ksj030120@gmail.com>
2024-03-18 04:20:38 +0900

    Create Hello_class.java
    Hello java 출력하기

---
+++ first/Hello_class.java
@@ -0,0 +1,10 @@
+ // hello_project/src/(default package)/Hello_class.java
+
+public class Hello_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello JAVA");
+	}
+
+}
