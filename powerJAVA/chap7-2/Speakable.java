5078e877ee79676acdeb82c2a57a5da95c214a3d
Sujin Kim <ksj030120@gmail.com>
2024-05-16 01:02:55 +0900

    Create Speakable.java
    인터페이스 생성 연습

---
+++ powerJAVA/chap7-2/Speakable.java
@@ -0,0 +1,9 @@
+//***************************
+// 파일명: Speakable.java
+// 작성자: 김수진
+// 작성일: 24.5.14.
+// 내용: Speakable 인터페이스 정의
+//***************************
+public interface Speakable {
+	void speak();
+}
