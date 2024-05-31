1ed92b0ccc9472c7be2e421eaa139313a82f1869
Sujin Kim <ksj030120@gmail.com>
2024-05-31 17:26:25 +0900

    Update MainThread.java

--- powerJAVA/chap16/hw20_1/MainThread.java
+++ powerJAVA/chap16/hw20_1/MainThread.java
@@ -6,9 +6,9 @@
 //***************************
 public class MainThread {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException { //throws로 sleep 예외처리
 		
-		Thread t1 = new Thread(new Thread1());
+		Thread t1 = new Thread(new Thread1()); //runnable 스레드(상속) 사용
 		Thread t2 = new Thread(new Thread2());
 
     // 스레드 시작
@@ -18,7 +18,7 @@ public class MainThread {
 		String msList[] =  {"1000", "2000", "3000", "4000"};
 		for (int i=0;i<msList.length;i++) {
 			System.out.println(msList[i]);
-			long randomMs = 1+(long)(Math.random()*1000);
+			long randomMs = 1+(long)(Math.random()*1000); // 랜덤 1~1000 사이 ms 
 			Thread.sleep(randomMs); //스레드 랜덤 ms만큼 기다림
 		}
 
