eaa30b1c0cc1f7147f655ab193a3bc43b3c29c00
Sujin Kim <ksj030120@gmail.com>
2024-05-31 17:22:10 +0900

    Create MainThread.java
    스레드 연습

---
+++ powerJAVA/chap16/hw20_1/MainThread.java
@@ -0,0 +1,33 @@
+//***************************
+// 파일명: MainThread.java
+// 작성자: 김수진
+// 작성일: 24.05.31.
+// 내용: 스레드 클래스 메소드들을 사용해본다. runnable, sleep, join사용
+//***************************
+public class MainThread {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		Thread t1 = new Thread(new Thread1());
+		Thread t2 = new Thread(new Thread2());
+
+    // 스레드 시작
+		t1.start();
+		t2.start();
+		
+		String msList[] =  {"1000", "2000", "3000", "4000"};
+		for (int i=0;i<msList.length;i++) {
+			System.out.println(msList[i]);
+			long randomMs = 1+(long)(Math.random()*1000);
+			Thread.sleep(randomMs); //스레드 랜덤 ms만큼 기다림
+		}
+
+		t1.join();//스레드1 종료까지 기다림
+		System.out.println("Thread1 종료");
+		t2.join();//스레드1 종료까지 기다림
+		System.out.println("Thread2 종료");
+	
+		
+	}
+
+}
