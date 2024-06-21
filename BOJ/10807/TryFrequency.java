03bf96674de776f475bbf08237e647dddf0e327b
Sujin Kim <ksj030120@gmail.com>
2024-06-22 04:29:29 +0900

    Update and rename BOJ/Main.java to BOJ/10807/TryFrequency.java

--- BOJ/10807/TryFrequency.java
+++ BOJ/10807/TryFrequency.java
@@ -1,10 +1,10 @@
-//
+//frequency 구현
 package p10807;
 
 import java.util.*;
 
 //설명: 총 n개의 정수가 주어졌을 때, 정수 v가 몇 개인지 구하기
-public class Main {
+public class TryFrequency {
 
 	public static void main(String[] args) {
 		//n 입력받기 
@@ -15,7 +15,7 @@ public class Main {
 		List<Integer> n_list = new ArrayList<>();
 		
 		for (int i=0; i<n; i++) {
-			n_list.add(sc.nextInt());
+			n_list.add(sc.nextInt()); //공백으로 구분하여 입력 or 줄바꿈으로 구분하여 입력
 		}
 
 		
@@ -23,8 +23,13 @@ public class Main {
 		int v = sc.nextInt();
 		
 		//n_list중 v 몇개인지 출력
-		System.out.println(Collections.frequency(n_list, v));
-		
+		int cnt=0;
+		for (int i:n_list) {
+			if (v==i) {
+				cnt+=1;
+			}
+		}
+		System.out.print(cnt);
 
 	}
 
